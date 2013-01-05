3c56014a8158455a6798a81013f8730cfd6bfca8
Marko Martinović <marko@techytalk.info>
2013-01-05 13:11:53 +0100

    Edit config/session.php

--- config/auth.php
+++ config/auth.php
@@ -4,7 +4,7 @@
 $config['auth_login_controller'] = 'login';
 
 // Hash key, change to invalidate all sessions and user registrations
-$config['auth_hash_key'] = '1234567';
+$config['auth_hash_key'] = '';
 
 /* End of file auth.php */
 /* Location: ./application/config/auth.php */
