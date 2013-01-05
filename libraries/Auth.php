6236f815bfa7c3fab433666eb15495ccb4c99be4
Marko Martinović <marko@techytalk.info>
2013-01-05 15:48:47 +0100

    phpdoc udpdate

--- libraries/Auth.php
+++ libraries/Auth.php
@@ -327,7 +327,9 @@ class Auth {
     protected function check_cookie($cookie) {
         $cookie_array = explode(' ', $cookie);
 
-        // $cookie_array is expected to have two elements, user_id and token hash
+        /* $cookie_array is expected to have two elements, user_id and
+         * plain token.
+         */
         if(empty($cookie_array) || count($cookie_array) < 2)
             return false;
 
