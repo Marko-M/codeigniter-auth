201d4add9260c6ab66d0547669bc3530773ae799
Marko Martinović <marko@techytalk.info>
2013-01-27 13:40:08 +0100

    Add features for token based registration

--- config/auth.php
+++ config/auth.php
@@ -4,20 +4,25 @@
 $config['auth_login_controller'] = 'login';
 
 /* Use something complicated. After initial setup change only when you need to
- * force all users to login again (after security breach).
+ * force all users to login again.
  */
 $config['auth_session_hash_key'] = '';
 
 /* Use something complicated. After initial setup change only when you need to
- * force all users to recover their passwords (after security breach).
+ * force all users to recover their passwords.
  */
 $config['auth_password_hash_key'] = '';
 
 
 /* Use something complicated. After initial setup change only when you need to
- * force all remember me option users to login again (after security breach).
+ * force all remember me option users to login again.
  */
 $config['auth_cookie_hash_key'] = '';
+
+/* Use something complicated. After initial setup change only when you need to
+ * force all not activated users to recover their activation emails.
+ */
+$config['auth_activate_hash_key'] = '';
 
 /* End of file auth.php */
 /* Location: ./application/config/auth.php */
